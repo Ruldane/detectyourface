5c876c8203b99cf79d9fa2366c5c92fc09cb7830
unknown <l.nizzoli@gmail.com>
2020-05-21 10:47:10 +0200

    first commit and front-end done

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {"Laurent, votre classement actuel est ..."}
+            </div>
+            <div className="white f3">
+                {"#5"}
+            </div>
+
+        </div>
+    )
+}
+
+export default Rank;
